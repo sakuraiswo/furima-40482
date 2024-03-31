7a6593f15038451d3581b9914804fa6c83e14a9b
sakuraiswo <sakuraisworks@gmail.com>
2024-04-01 05:54:45 +0900

    rubocopの自動修正実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.name}
-    email {Faker::Internet.email}
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name { "ミョう字" }
-    first_name { "ナま絵" }
-    last_name_kana { "ミョウジ" }
-    first_name_kana { "ナマエ" }
-    birthday {Faker::Date.between(from: '1930-01-01', to: '2019-12-31')}
-
-
+    nickname { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name { 'ミョう字' }
+    first_name { 'ナま絵' }
+    last_name_kana { 'ミョウジ' }
+    first_name_kana { 'ナマエ' }
+    birthday { Faker::Date.between(from: '1930-01-01', to: '2019-12-31') }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,14 +5,13 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
       it ' nickname、email、password、password_confirmation、last_name、first_name、last_name_kana、first_name_kana、birthdayが存在すれば登録できる' do
         expect(@user).to be_valid
       end
     end
-  
+
     context '新規登録できない場合' do
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
@@ -70,7 +69,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Email is invalid')
       end
-       it 'passwordが5文字以下では登録できない' do
+      it 'passwordが5文字以下では登録できない' do
         @user.password = '00000'
         @user.password_confirmation = '00000'
         @user.valid?
@@ -80,71 +79,70 @@ RSpec.describe User, type: :model do
         @user.password = Faker::Internet.password(min_length: 129, max_length: 150)
         @user.password_confirmation = @user.password
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")     
+        expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
       end
       it 'passwordは数字だけでは登録できない' do
         @user.password = '000000'
         @user.password_confirmation = '000000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password must include both letters and numbers.")
+        expect(@user.errors.full_messages).to include('Password must include both letters and numbers.')
       end
       it 'passwordは英字だけでは登録できない' do
         @user.password = 'aaaaaa'
         @user.password_confirmation = 'aaaaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password must include both letters and numbers.")
+        expect(@user.errors.full_messages).to include('Password must include both letters and numbers.')
       end
       it 'last_nameが半角では登録できない' do
         @user.last_name = 'ﾐｮｳジ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name must consist of full-width characters only.")
+        expect(@user.errors.full_messages).to include('Last name must consist of full-width characters only.')
       end
       it 'last_nameが全角でも英字では登録できない' do
         @user.last_name = 'Ｎａｍｅ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name must consist of full-width characters only.")
+        expect(@user.errors.full_messages).to include('Last name must consist of full-width characters only.')
       end
       it 'first_nameが半角では登録できない' do
         @user.first_name = 'ﾅﾏｴ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name must consist of full-width characters only.")
+        expect(@user.errors.full_messages).to include('First name must consist of full-width characters only.')
       end
       it 'first_nameが全角でも英字では登録できない' do
         @user.first_name = 'Ｎａｍｅ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name must consist of full-width characters only.")
+        expect(@user.errors.full_messages).to include('First name must consist of full-width characters only.')
       end
       it 'last_name_kanaが半角では登録できない' do
         @user.last_name_kana = 'ﾐｮｳｼﾞ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name kana must consist of full-width Katakana characters only.")
+        expect(@user.errors.full_messages).to include('Last name kana must consist of full-width Katakana characters only.')
       end
       it 'last_name_kanaがひらがなでは登録できない' do
         @user.last_name_kana = 'みょうじ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name kana must consist of full-width Katakana characters only.")
+        expect(@user.errors.full_messages).to include('Last name kana must consist of full-width Katakana characters only.')
       end
       it 'last_name_kanaが漢字では登録できない' do
         @user.last_name_kana = '苗字'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name kana must consist of full-width Katakana characters only.")
+        expect(@user.errors.full_messages).to include('Last name kana must consist of full-width Katakana characters only.')
       end
       it 'first_name_kanaが半角では登録できない' do
         @user.first_name_kana = 'ﾅﾏｴ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana must consist of full-width Katakana characters only.")
+        expect(@user.errors.full_messages).to include('First name kana must consist of full-width Katakana characters only.')
       end
       it 'first_name_kanaがひらがなでは登録できない' do
         @user.first_name_kana = 'なまえ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana must consist of full-width Katakana characters only.")
+        expect(@user.errors.full_messages).to include('First name kana must consist of full-width Katakana characters only.')
       end
       it 'first_name_kanaが漢字では登録できない' do
         @user.first_name_kana = '名前'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana must consist of full-width Katakana characters only.")
+        expect(@user.errors.full_messages).to include('First name kana must consist of full-width Katakana characters only.')
       end
-
     end
   end
 end
