12a876c994a84bff4a7a9202fbc66eae94b63f12
sakuraiswo <sakuraisworks@gmail.com>
2024-04-02 02:00:37 +0900

    ActiveHashの修正と画像アップロードの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,23 @@ class ItemsController < ApplicationController
   def index
     @items = Item.order("created_at DESC")
   end
+
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:imege, :item_name, :introduction, :category_id, :condition_id, :delivery_charge_burden_id, :prefecture_id, :number_of_day_id, :price).merge(user_id: current_user.id)
   end
 
 end
